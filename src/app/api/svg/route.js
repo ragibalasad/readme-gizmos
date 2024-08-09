e29869caecc8d8b5e93ca692c72ecce6b6e7cfe0
Ragib Al Asad <ragibalasad@gmail.com>
2024-08-10 00:42:39 +0600

    Update api/svg/route.js

--- src/app/api/svg/route.js
+++ src/app/api/svg/route.js
@@ -2,46 +2,19 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  // Define your logos list with URLs relative to the public directory
-  const logos = [
-    { url: '/images/html.png', title: 'HTML Logo', x: 0 },
-    { url: '/images/css.png', title: 'CSS Logo', x: 200 },
-    { url: '/images/js.png', title: 'JavaScript Logo', x: 400 }
-  ];
-
-  const logoImages = logos.map((logo, index) => `
-    <image
-      id="logo-${index + 1}"
-      class="logo logo-${(index % 3) + 1}"
-      href="${logo.url}"
-      x="${logo.x}"
-      y="50"
-      width="100"
-      height="100"
-      title="${logo.title}"
-    />
-    <image
-      id="logo-${index + logos.length + 1}"
-      class="logo logo-${(index % 3) + 1}"
-      href="${logo.url}"
-      x="${logo.x + 600}"
-      y="50"
-      width="100"
-      height="100"
-      title="${logo.title}"
-    />
-  `).join('');
-
   const svgContent = `
-    <svg id="logo-svg" width="1200" height="200" xmlns="http://www.w3.org/2000/svg">
+    <svg width="1200" height="200" xmlns="http://www.w3.org/2000/svg">
       <g class="slider">
-        ${logoImages}
+        <image href="/images/html.png" x="0" y="50" width="100" height="100" />
+        <image href="/images/css.png" x="200" y="50" width="100" height="100" />
+        <image href="/images/js.png" x="400" y="50" width="100" height="100" />
       </g>
     </svg>
   `;
 
   const headers = new Headers({
     'Content-Type': 'image/svg+xml',
+    'Cache-Control': 'no-cache'
   });
 
   return new NextResponse(svgContent, { headers });
