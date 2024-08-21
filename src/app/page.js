2c3c5eff14b2e60da7ce1c667eef020e6168d91b
Ragib Al Asad <ragibalasad@gmail.com>
2024-08-21 17:06:45 +0600

    Added badge proxy server | Fix

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,6 @@
 import React from "react";
 
 export default async function Home() {
-  const response = await fetch(
-    "http://localhost:3000/github/stars/ragibalasad?color=55960c&label=Stars&labelColor=488207&style=flat&logo=star",
-  );
-  const svgData = await response.text();
-
   return (
     <main>
       <div dangerouslySetInnerHTML={{ __html: svgData }} />
